7f02de5437ca3f8d9664f71e356829e1ca655e6e
Shiva <77195609+shivaghanbari@users.noreply.github.com>
2024-05-12 16:38:48 +0330

    Update my-house.py

--- my-house.py
+++ my-house.py
@@ -1,81 +1,49 @@
-from colorama import Fore, Back, Style
+from colorama import Back, Style
 
-# Function to draw the ceiling of the house
-def ceiling():
-    # Top part of the ceiling
-    print(Back.LIGHTBLUE_EX + " " * 37 + Back.YELLOW + " " * 6 + Back.LIGHTBLUE_EX + " " * 37 + Style.RESET_ALL)
-    # Middle part of the ceiling
-    print(Back.LIGHTBLUE_EX + " " * 35 + Back.YELLOW + " " * 10 + Back.LIGHTBLUE_EX + " " * 35 + Style.RESET_ALL)
-    # Bottom part of the ceiling
-    print(Back.LIGHTBLUE_EX + " " * 30 + Back.YELLOW + " " * 20 + Back.LIGHTBLUE_EX + " " * 30 + Style.RESET_ALL)
-    print(Back.LIGHTBLUE_EX + " " * 30 + Back.YELLOW + " " * 20 + Back.LIGHTBLUE_EX + " " * 30 + Style.RESET_ALL)
-    print(Back.LIGHTBLUE_EX + " " * 25 + Back.YELLOW + " " * 30 + Back.LIGHTBLUE_EX + " " * 25 + Style.RESET_ALL)
-    print(Back.LIGHTBLUE_EX + " " * 25 + Back.YELLOW + " " * 30 + Back.LIGHTBLUE_EX + " " * 25 + Style.RESET_ALL)
-    print(Back.LIGHTBLUE_EX + " " * 25 + Back.YELLOW + " " * 30 + Back.LIGHTBLUE_EX + " " * 25 + Style.RESET_ALL)
-    print(Back.LIGHTBLUE_EX + " " * 20 + Back.YELLOW + " " * 40 + Back.LIGHTBLUE_EX + " " * 20 + Style.RESET_ALL)
-    print(Back.LIGHTBLUE_EX + " " * 20 + Back.YELLOW + " " * 40 + Back.LIGHTBLUE_EX + " " * 20 + Style.RESET_ALL)
-    print(Back.LIGHTBLUE_EX + " " * 15 + Back.YELLOW + " " * 50 + Back.LIGHTBLUE_EX + " " * 15 + Style.RESET_ALL)
-    print(Back.LIGHTBLUE_EX + " " * 15 + Back.YELLOW + " " * 50 + Back.LIGHTBLUE_EX + " " * 15 + Style.RESET_ALL)
+def draw_rectangle(width, height, color):
+    for _ in range(height):
+        print(color + " " * width + Style.RESET_ALL)
 
-# Function to draw the body of the house
-def body():
-    i = 1
-    while i < 5:
-        print(Back.LIGHTBLUE_EX + " " * 15 + Back.RED + " " * 50 + Back.LIGHTBLUE_EX + " " * 15 + Style.RESET_ALL)
-        i += 1
+def draw_ceiling():
+    print(Back.LIGHTBLUE_EX + " " * 37 + Back.YELLOW + " " * 6 + " " * 37 + Style.RESET_ALL)
+    print(Back.LIGHTBLUE_EX + " " * 35 + Back.YELLOW + " " * 10 + " " * 35 + Style.RESET_ALL)
+    print(Back.LIGHTBLUE_EX + " " * 30 + Back.YELLOW + " " * 20 + " " * 30 + Style.RESET_ALL)
+    print(Back.LIGHTBLUE_EX + " " * 25 + Back.YELLOW + " " * 30 + " " * 25 + Style.RESET_ALL)
+    print(Back.LIGHTBLUE_EX + " " * 20 + Back.YELLOW + " " * 40 + " " * 20 + Style.RESET_ALL)
+    print(Back.LIGHTBLUE_EX + " " * 15 + Back.YELLOW + " " * 50 + " " * 15 + Style.RESET_ALL)
 
-# Function to draw the window of the house
-def window():
-    i = 1
-    while i < 5:
-        print(
-            Back.LIGHTBLUE_EX + " " * 15 + Back.RED + " " * 5 + Back.WHITE + " " * 10 + Back.RED + " " * 20 + Back.WHITE + " " * 10 + Back.RED + " " * 5 + Back.LIGHTBLUE_EX + " " * 15 + Style.RESET_ALL)
-        i += 1
+def draw_body():
+    draw_rectangle(80, 4, Back.LIGHTBLUE_EX + " " * 15 + Back.RED)
 
-# Function to draw the door of the house
-def door():
-    i = 1
-    while i < 6:
-        print(
-            Back.LIGHTBLUE_EX + " " * 15 + Back.RED + " " * 15 + Back.YELLOW + " " * 9 + Back.LIGHTBLACK_EX + " " * 1 + Back.YELLOW + " " * 10 + Back.RED + " " * 15 +
-            Back.LIGHTBLUE_EX + " " * 15 + Style.RESET_ALL)
-        i += 1
+def draw_window():
+    for _ in range(4):
+        print(Back.LIGHTBLUE_EX + " " * 15 + Back.RED + " " * 5 + Back.WHITE + " " * 10 + Back.RED + " " * 20 + Back.WHITE + " " * 10 + Back.RED + " " * 5 + " " * 15 + Style.RESET_ALL)
 
-# Function to draw the door handle
-def handler():
-    i = 0
-    while i < 2:
-        print(
-            Back.LIGHTBLUE_EX + " " * 15 + Back.RED + " " * 15 + Back.YELLOW + " " * 8 + Back.LIGHTBLACK_EX + " " * 3 +
-            Back.YELLOW + " " * 9 + Back.RED + " " * 15 + Back.LIGHTBLUE_EX + " " * 15 + Style.RESET_ALL)
-        i += 1
+def draw_door():
+    for _ in range(5):
+        print(Back.LIGHTBLUE_EX + " " * 15 + Back.RED + " " * 15 + Back.YELLOW + " " * 9 + Back.LIGHTBLACK_EX + " " * 1 + Back.YELLOW + " " * 10 + Back.RED + " " * 15 + " " * 15 + Style.RESET_ALL)
 
-# Function to draw the grass
-def grass():
-    i = 1
-    while i < 3:
+def draw_door_handle():
+    for _ in range(2):
+        print(Back.LIGHTBLUE_EX + " " * 15 + Back.RED + " " * 15 + Back.YELLOW + " " * 8 + Back.LIGHTBLACK_EX + " " * 3 + Back.YELLOW + " " * 9 + Back.RED + " " * 15 + " " * 15 + Style.RESET_ALL)
+
+def draw_grass():
+    for _ in range(5):
         print(Back.GREEN + " " * 100 + Style.RESET_ALL)
-        i += 1
 
-# Function to draw a flower
-def flower():
-    print(Back.GREEN + " " * 20 + Back.LIGHTCYAN_EX + " " * 2 + Back.GREEN + " " * 20 + Back.LIGHTCYAN_EX + " " * 2 +
-          Back.GREEN + " " * 20 + Back.LIGHTCYAN_EX + " " * 2 + Back.GREEN + " " * 34 + Style.RESET_ALL)
-    print(
-        Back.GREEN + " " * 18 + Back.LIGHTCYAN_EX + " " * 2 + Back.YELLOW + " " * 2 + Back.LIGHTCYAN_EX + " " * 2 + Back.GREEN + " " * 16 + Back.LIGHTCYAN_EX + " " * 2 +
-        Back.YELLOW + " " * 2 + Back.LIGHTCYAN_EX + " " * 2 + Back.GREEN + " " * 16 + Back.LIGHTCYAN_EX + " " * 2 + Back.YELLOW + " " * 2 +
-        Back.LIGHTCYAN_EX + " " * 2 + Back.GREEN + " " * 32 + Style.RESET_ALL)
-    print(Back.GREEN + " " * 20 + Back.LIGHTCYAN_EX + " " * 2 + Back.GREEN + " " * 20 + Back.LIGHTCYAN_EX + " " * 2 +
-          Back.GREEN + " " * 20 + Back.LIGHTCYAN_EX + " " * 2 + Back.GREEN + " " * 34 + Style.RESET_ALL)
+def draw_flower():
+    print(Back.GREEN + " " * 20 + Back.LIGHTCYAN_EX + " " * 2 + " " * 20 + Back.LIGHTCYAN_EX + " " * 2 + " " * 20 + Back.LIGHTCYAN_EX + " " * 2 + " " * 34 + Style.RESET_ALL)
+    print(Back.GREEN + " " * 18 + Back.LIGHTCYAN_EX + " " * 2 + Back.YELLOW + " " * 2 + Back.LIGHTCYAN_EX + " " * 2 + " " * 16 + Back.LIGHTCYAN_EX + " " * 2 + Back.YELLOW + " " * 2 + Back.LIGHTCYAN_EX + " " * 2 + " " * 16 + Back.LIGHTCYAN_EX + " " * 2 + Back.YELLOW + " " * 2 + Back.LIGHTCYAN_EX + " " * 2 + " " * 32 + Style.RESET_ALL)
+    print(Back.GREEN + " " * 20 + Back.LIGHTCYAN_EX + " " * 2 + " " * 20 + Back.LIGHTCYAN_EX + " " * 2 + " " * 20 + Back.LIGHTCYAN_EX + " " * 2 + " " * 34 + Style.RESET_ALL)
 
-# Drawing the elements of the house
-ceiling()
-body()
-window()
-body()
-door()
-handler()
-door()
-grass()
-flower()
-grass()
+# Drawing the house
+draw_ceiling()
+draw_body()
+draw_window()
+draw_body()
+draw_door()
+draw_door_handle()
+draw_door()
+draw_grass()
+draw_flower()
+draw_grass()
